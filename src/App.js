9a38f86a4383ea42e7c27327189b9fe06fba48c5
Dario Turin <dturin1997@gmail.com>
2023-02-14 17:59:51 -0500

    Video-10-udemy: Create Routes For Publicly Available Pages Like List Of Products

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import ProductListPage from "./pages/ProductListPage";
+import CartPage from "./pages/CartPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product-list" element={<ProductListPage />} />
+        <Route path="/product-details" element={<ProductDetailsPage />} />
+        <Route path="/product-details/:id" element={<ProductDetailsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element="Page not exists 404" />
+        {/* <Route path="/" component={HomePage} />  in previous versions of react-router-dom */}
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
